621f19e8bfd19bc957a7308877919543b23c3edf
CtrlAltKismet <brookesdet@gmail.com>
2026-07-20 18:52:15 +0100

    Set up django skeleton framework for website.

---
+++ walkies_and_whiskers/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for walkies_and_whiskers project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'walkies_and_whiskers.settings')
+
+application = get_wsgi_application()
