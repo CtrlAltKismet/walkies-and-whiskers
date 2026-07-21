1b84bc1c6546bbf9a4b7a5a8020a2018f6c3130d
CtrlAltKismet <brookesdet@gmail.com>
2026-07-21 13:05:25 +0100

    Added about html in templates

--- home/views.py
+++ home/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def home(request):
     """Display the homepage."""
     return render(request, "home/index.html")
+
+def about(request):
+    """Display the about page."""
+    return render(request, "home/about.html")
