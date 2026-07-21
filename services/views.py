a414df95d5d4005a07660752b027fd76b04ddeb6
CtrlAltKismet <brookesdet@gmail.com>
2026-07-21 13:22:24 +0100

    Added service list to views.py

--- services/views.py
+++ services/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Service
+
+
+def service_list(request):
+    """Display all active pet-care services."""
+    services = Service.objects.filter(is_active=True)
+    
+    context = {
+        "services": services,
+    }
+    
+    return render(request, "services/service_list.html", context)
