527b9c1c60a4b26e659801af474fbf43a64d58e6
CtrlAltKismet <brookesdet@gmail.com>
2026-07-21 14:18:30 +0100

    Added register, log in and log out paths to account urls.py

---
+++ accounts/urls.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import views as auth_views
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path(
+        "register/",
+        views.register,
+        name="register",
+    ),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="acounts/login.html",
+            redirect_authenticated_user=True,
+        ),
+        name="login",
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(),
+        name="logout",
+    ),
+]
