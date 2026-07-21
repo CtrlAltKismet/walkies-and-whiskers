62f1837d5b30832e4cacb03ff64212431a463a85
CtrlAltKismet <brookesdet@gmail.com>
2026-07-21 15:55:42 +0100

    Added email settings to settings.py for confirmation emails

--- walkies_and_whiskers/settings.py
+++ walkies_and_whiskers/settings.py
@@ -138,3 +138,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+# Email settings
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+DEFAULT_FROM_EMAIL = "Walkies & Whiskers <noreply@walkiesandwhiskers.com>"
