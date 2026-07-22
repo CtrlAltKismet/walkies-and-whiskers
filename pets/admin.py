802747ece3b3d0b5edcaf26f041d5930340c3b68
CtrlAltKismet <brookesdet@gmail.com>
2026-07-22 13:43:12 +0100

    configured pet profiles in django admin

--- pets/admin.py
+++ pets/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Pet
+
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    """Configure pet profiles in Django admin."""
+    
+    list_display = (
+        "name",
+        "species",
+        "breed",
+        "size",
+        "owner",
+        "created_at",
+    )
+    list_filter = (
+        "species",
+        "size",
+    )
+    search_fields = (
+        "name",
+        "species",
+        "breed",
+        "owner_username",
+    )
