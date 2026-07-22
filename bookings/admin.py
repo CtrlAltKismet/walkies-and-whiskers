013706d3ea3c680313d185a78125d592efeae37b
CtrlAltKismet <brookesdet@gmail.com>
2026-07-22 18:00:42 +0100

    Add booking admin display and filters

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Booking
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    """Configure bookings in Django admin."""
+    
+    list_display = (
+        "id",
+        "user",
+        "pet",
+        "service",
+        "booking_date",
+        "booking_time",
+        "status",
+        "total_price",
+        "created_at",
+    )
+    list_filter = (
+        "status",
+        "service",
+        "booking_date",
+    )
+    search_fields = (
+        "user__username",
+        "pet__name",
+        "service__name",
+    )
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
