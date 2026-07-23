85e3a76de59b3cccf47fbcd27b3ba118da5068f4
CtrlAltKismet <brookesdet@gmail.com>
2026-07-23 11:32:07 +0100

    Added payment success url to checkout urls

--- checkout/urls.py
+++ checkout/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
         views.create_checkout_session,
         name="create_checkout_session",
     ),
+    path(
+        "success/",
+        views.payment_success,
+        name="payment_success",
+    ),
 ]
