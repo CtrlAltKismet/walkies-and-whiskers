e321b4cd318263fb48567463af7bb77eee3b7120
CtrlAltKismet <brookesdet@gmail.com>
2026-07-23 11:30:28 +0100

    Added orders to django admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """Configure Stripe payment records in Django Admin."""
+    
+    list_display = (
+        "id",
+        "booking",
+        "user",
+        "amount",
+        "status",
+        "stripe_checkout_id",
+        "created_at",
+    )
+    list_filter = (
+        "status",
+        "created_at",
+    )
+    search_fields = (
+        "booking__pet__name",
+        "booking__service__name",
+        "user__username",
+        "stripe_checkout_id",
+        "stripe_payment_intent",
+    )
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
