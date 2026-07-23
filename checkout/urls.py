43fed71fcd2f619538b565da99f9e15556071c20
CtrlAltKismet <brookesdet@gmail.com>
2026-07-23 10:42:55 +0100

    Create checkout session added to checkout urls

---
+++ checkout/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path(
+        "<int:booking_id>/",
+        views.create_checkout_session,
+        name="create_checkout_session",
+    ),
+]
