4ba40277eedebedee71886207b147fcbfcfdcc4f
CtrlAltKismet <brookesdet@gmail.com>
2026-07-29 16:53:44 +0100

    Configure productio nemail delivery

--- walkies_and_whiskers/settings.py
+++ walkies_and_whiskers/settings.py
@@ -177,6 +177,27 @@ LOGOUT_REDIRECT_URL = "home"
 
 # Email settings
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
-DEFAULT_FROM_EMAIL = "Walkies & Whiskers <noreply@walkiesandwhiskers.com>"
+if EMAIL_HOST_USER and EMAIL_HOST_PASSWORD:
+    EMAIL_BACKEND = (
+        "django.core.mail.backends.smtp.EmailBackend"
+    )
+    EMAIL_HOST = "smtp.gmail.com"
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_TIMEOUT = 20
+
+    DEFAULT_FROM_EMAIL = (
+        f"Walkies & Whiskers <{EMAIL_HOST_USER}>"
+    )
+else:
+    EMAIL_BACKEND = (
+        "django.core.mail.backends.console.EmailBackend"
+    )
+
+    DEFAULT_FROM_EMAIL = (
+        "Walkies & Whiskers "
+        "<noreply@walkiesandwhiskers.com>"
+    )
