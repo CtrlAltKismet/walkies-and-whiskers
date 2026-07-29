30d6b934e5e970615d641c7dd40b0fb55728f200
CtrlAltKismet <brookesdet@gmail.com>
2026-07-29 15:21:45 +0100

    Added custom 404 to views

--- home/views.py
+++ home/views.py
@@ -5,6 +5,17 @@ def home(request):
     """Display the homepage."""
     return render(request, "home/index.html")
 
+
 def about(request):
     """Display the about page."""
     return render(request, "home/about.html")
+
+
+def custom_404(request, exception):
+    """Display the custom page-not-found response."""
+
+    return render(
+        request,
+        "404.html",
+        status=404,
+    )
