85ef28017d42e7378705c267ec2d1eff4bb27b02
CtrlAltKismet <brookesdet@gmail.com>
2026-08-05 16:22:02 +0100

    Corrected PEP8 for forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,8 +9,7 @@ from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
     """Create a new Walkies & Whiskers user account."""
-    
-    
+
     class Meta:
         model = User
         fields = (
@@ -20,30 +19,34 @@ class RegisterForm(UserCreationForm):
             "password2",
         )
 
+
 class CustomPasswordChangeForm(PasswordChangeForm):
     """Prevent users from reusing their current password."""
-    
+
     def clean_new_password1(self):
         """Check that the new password differs from the old password."""
         new_password = self.cleaned_data.get("new_password1")
-        
+
         if new_password and self.user.check_password(new_password):
             raise forms.ValidationError(
-                "Your new password must be different from your current password."
+                "Your new password must be different from your "
+                "current password."
             )
-            
+
         return new_password
-    
+
+
 class CustomSetPasswordForm(SetPasswordForm):
     """Prevent password reuse during an email password reset."""
-    
+
     def clean_new_password1(self):
         """Check that the new password differs from the current password."""
         new_password = self.cleaned_data.get("new_password1")
-        
+
         if new_password and self.user.check_password(new_password):
             raise forms.ValidationError(
-                "Your new password must be different from your current password."
+                "Your new password must be different from your "
+                "current password."
             )
-        
+
         return new_password
